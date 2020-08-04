06bb8e81ab30cb9e739a0e42ed9e095deb827568
kims man <zero12a@gmail.com>
2020-08-04 22:12:33 +0900

    jodit

---
+++ demo_jodit.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jodit/3.4.15/jodit.min.css"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jodit/3.4.15/jodit.min.js"></script>
+</head>
+<body>
+1
+<textarea id="editor" name="editor"></textarea>
+2
+
+<script>
+var editor = new Jodit('#editor',{
+    enableDragAndDropFileToEditor: true,
+    uploader: {
+        url: 'demo_jodit_upload.php?action=fileUpload',
+        format: 'json',
+        imagesExtensions: ["jpg", "png", "jpeg", "gif"],
+        method: "POST",
+        error: function(e){
+            alog("error...............start");
+            alog(e);
+            this.j.e.fire("errorMessage",e.message,"error",4e3);
+        }
+    },
+});
+editor.value = '<p>start</p>';
+
+function alog(t){if(console)console.log(t);}
+</script>
+</body>
+</html>
