7eb8968e8f188614fb6103f4976b7e4faaae2fb4
kims man <zero12a@gmail.com>
2022-05-19 21:49:31 +0900

    .

--- coding/std_fileview.php
+++ coding/std_fileview.php
@@ -17,6 +17,10 @@
     </style>
     <script>
         function init(){
+            reload();
+        }
+
+        function reload(e,t){
             $.ajax({
                 url: "sbfilemng/sbfilemng.php?CMD=list&PATH=/",
                 dataType: "json",
@@ -26,12 +30,17 @@
                 alog(data);
                 //alert(data);
                 ul = $("#fileRoot");
+                ul.empty();//비우기
                 for(i=0;i<data.length;i++){
+                    //var li=document.createElement('li');
                     if(data[i].dir == "Y"){
                         ul.append(mkFoldTag(this.privitePath, data[i].nm)); //ul_list안쪽에 li추가
+                        //li.innerHtml = mkFoldTag(this.privitePath, data[i].nm);
                     }else{
                         ul.append(mkFileTag(this.privitePath, data[i].nm));
+                        //li.innerHtml = mkFileTag(this.privitePath, data[i].nm);
                     }
+                    //ul.appendChild(li);
                 }
 
             })
@@ -45,7 +54,7 @@
             //alog("  끝이 뭐냐 : = " + path.slice(-1));
             if(path.slice(-1,1) != "/")path = path + "/";
             //alog("  변환함 = " + path);
-            return "<li onclick='viewChildList(event, \"" + path + nm + "\" ,this);'> <i class='fa-solid fa-caret-right'></i> <i class='fa-solid fa-folder' style='margin-right:10px;'></i>" + nm + "</li>";                    
+            return "<li onclick='viewChildList(event, \"" + path + nm + "\" ,this);'> <i class='fa-solid fa-caret-right' onclick='fold(event,this)'></i> <i class='fa-solid fa-folder' style='margin-right:10px;'></i>" + nm + "</li>";                    
         }
         function mkFileTag(path,nm){
             if(path.slice(-1,1) != "/")path = path + "/";
@@ -56,7 +65,55 @@
             alert("viewFile() " + path + file);
         }
 
+        function fold(e,iObj){
+            //alog("fold()................start");
+            //alog(iObj);
+
+            //caret-right 아이콘 회전
+            if($(iObj).hasClass("fa-rotate-90")){
+                $(iObj).removeClass("fa-rotate-90");
+            }else{
+                $(iObj).addClass("fa-rotate-90");
+            }
+
+            liObj = iObj.parentNode;
+            //중북 클릭 이벤트방지
+            var evt = e ? e:window.event;
+            if (evt.stopPropagation)    evt.stopPropagation();
+            if (evt.cancelBubble!=null) evt.cancelBubble = true;
+
+            if (liObj.hasChildNodes()){
+                //alog(111);
+                // 그래서, 먼저 개체가 찼는 지(자식 노드가 있는 지) 검사
+                var isHaveChild = false;
+                var children = liObj.childNodes;
+                for (var i = 0; i < children.length; i++){
+                    // children[i]로 각 자식에 무언가를 함
+                    // 주의: 목록은 유효해(live), 자식 추가나 제거는 목록을 바꿈
+                    //alog(i + "---------------");
+                    //alog(children[i])
+                    if(children[i].nodeName == "UL") {
+                        //alog("차일드 UL이 이미 있음");
+                        //children[i].remove();
+
+                        childUl = $(children[i]);
+
+                        if( childUl.css("display") == "none" ){
+                            childUl.show();
+                        }else{
+                            childUl.hide();
+                        }
+                    }
+                }
+            }else{
+                //alog(222)
+            }
+    
+
+        }
+
         function viewChildList(e, path, liObj){
+            alog("viewChildList()................start");
             //중북 클릭 이벤트방지
             var evt = e ? e:window.event;
             if (evt.stopPropagation)    evt.stopPropagation();
@@ -82,20 +139,7 @@
                     }
                 }
             }
-    
-            /*
-            if(liObj.lastChild) {
-                alog("liObj - remove");
-                alog(liObj.lastChild);
-                if(liObj.lastChild.nodeName == "UL"){
-                    liObj.removeChild(liObj.firstChild);
-                }else{
-                    alog("text node는 삭제하지 않음");
-                }
 
-            }
-            */
-            //return;
 
             $.ajax({
                 url: "sbfilemng/sbfilemng.php?CMD=list&PATH=" + path,
@@ -107,18 +151,24 @@
                 alog(data);
                 //alert(data);
                 //ul을 먼저 추가
-                var ul=document.createElement('ul');
+                //var ul=document.createElement('ul');
+                var ul = $("<ul></ul>");
                 for(i=0;i<data.length;i++){
-                    var li=document.createElement('li');
+                    //var li=document.createElement('li');
 
                     if(data[i].dir == "Y"){
-                        li.innerHTML = mkFoldTag(this.privitePath, data[i].nm);
+                        //li.innerHTML = mkFoldTag(this.privitePath, data[i].nm);
+                        ul.append(mkFoldTag(this.privitePath, data[i].nm));
                     }else{
-                        li.innerHTML = mkFileTag(this.privitePath, data[i].nm);
+                        //li.innerHTML = mkFileTag(this.privitePath, data[i].nm);
+                        ul.append( mkFileTag(this.privitePath, data[i].nm));
                     }
-                    ul.appendChild(li);
+                    //ul.appendChild(li);
                 }
-                this.privateLiObj.appendChild(ul);
+                //this.privateLiObj.appendChild(ul);
+                alog(ul);
+                //this.privateLiObj.append(ul[0].innerHTML);
+                $(this.privateLiObj).append(ul);
 
             })
             .fail(function(xhr, status, errorThrown) { 
@@ -133,8 +183,11 @@
     </head>
 <body onload="init();">
 file viewer
-<input type="button" value='reload'><input type="button" value="add file"><input type="button" value="add folder">
-        <input type="button" value="delete">
-        <ul id="fileRoot"></ul>
+<i class='fa-solid fa-arrow-rotate-right' onclick='reload(event,this);'></i>
+<i class='fa-solid fa-file-circle-plus' onclick='reload(event,this);'></i> 
+<i class='fa-solid fa-folder-plus' onclick='reload(event,this);'></i> 
+<i class='fa-solid fa-trash-can' onclick='reload(event,this);'></i> 
+
+<ul id="fileRoot"></ul>
 </body>
 </html>
